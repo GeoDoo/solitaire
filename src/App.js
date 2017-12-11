6e2ddb7b05667f37056715418e2375dfafd442ca
Georgios Karametas <geometas@hotmail.com>
2017-12-11 16:44:44 +0100

    Cleaning up and displaying the first card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './libs/css/cards.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="playingCards fourColours faceImages simpleCards inText rotateHand">
+        <ul>
+          <li>
+            <div class="card rank-7 spades">
+                <span class="rank">7</span>
+                <span class="suit">&spades;</span>
+            </div>
+          </li>
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
