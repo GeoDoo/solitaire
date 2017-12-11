6b4146cfaebc2ee0b7ce33c743fb1804c8c1a128
Georgios Karametas <geometas@hotmail.com>
2017-12-11 20:27:00 +0100

    Fixed ASCII problem

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
-const Card = () => {
+const ascii = kind => {
+	switch(kind) {
+		case 'diams':
+			return '♦'
+		case 'hearts':
+			return '♥'
+		case 'spades':
+			return '♣'
+		case 'clubs':
+			return '♠'
+	}
+}
+
+const Card = (props) => {
 	return (
-		<div className="card rank-7 spades">
-	      <span className="rank">7</span>
-	      <span className="suit">&spades;</span>
+		<div className={`card rank-${props.rank} ${props.kind}`}>
+	      <span className="rank">{props.rank.toUpperCase()}</span>
+	      <span className="suit">{ascii(props.kind)}</span>
 	  </div>
 	)
 }
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,63 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Card from './Card'
 
-const Deck = () => {
-	return (
-		<ul>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-			<li><Card /></li>
-		</ul>
-	)
+class Deck extends Component {
+	renderCards() {
+		const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a' ]
+		const deck = {
+			"diams": ranks,
+			"hearts": ranks,
+			"spades": ranks,
+			"clubs": ranks
+		}
+
+		let cardList = []
+
+		for (let kind in deck) {
+			cardList.push(
+				deck[kind].map((card) => {
+					return <li key={`card-${kind}-${card}`}><Card kind={kind} rank={card} /></li>
+				})
+			)
+		}
+
+		return cardList
+	}
+
+	render() {
+		return (
+			<ul>
+				{this.renderCards()}
+			</ul>
+		)
+	}
 }
 
 export default Deck
